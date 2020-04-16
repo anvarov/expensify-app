9daf897a35f0a64f2b0726e3b787bda743c10ad9
Akmal <akmalanvaroff@gmail.com>
2020-04-16 19:55:18 +0500

    added named export for testin purposes

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,25 +4,28 @@ import PropType from "prop-types";
 import ExpenseListItem from "./ExpenseListItem";
 import selectExpenses from "../selectors/expenses";
 
-const ExpenseList = (props) => {
+export const ExpenseList = (props) => {
   const { expenses } = props;
 
   return (
     <div>
-      <h1>Expense List</h1>
-      {expenses.map((expense) => {
-        const { description, id, note, amount, createdAt } = expense;
-        return (
-          <ExpenseListItem
-            key={id}
-            description={description}
-            note={note}
-            createdAt={createdAt}
-            amount={amount}
-            id={id}
-          />
-        );
-      })}
+      {expenses.length === 0 ? (
+        <p>No Expenses</p>
+      ) : (
+        expenses.map((expense) => {
+          const { description, id, note, amount, createdAt } = expense;
+          return (
+            <ExpenseListItem
+              key={id}
+              description={description}
+              note={note}
+              createdAt={createdAt}
+              amount={amount}
+              id={id}
+            />
+          );
+        })
+      )}
     </div>
   );
 };
