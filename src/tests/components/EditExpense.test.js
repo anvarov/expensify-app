d86a186e40c68ebcae16b2b3912432a180c37c54
Akmal <akmalanvaroff@gmail.com>
2020-04-17 19:57:15 +0500

    written test for editexpensepage

--- src/tests/components/EditExpense.test.js
+++ src/tests/components/EditExpense.test.js
@@ -1,24 +1,39 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { EditExpensePage } from "../../components/EditExpense";
+import ExpenseForm from "../../components/ExpenseForm";
 import expenses from "../fixtures/expenses";
 
-test("should render edit expense page correctly", () => {
-  const wrapper = shallow(<EditExpensePage expense={expenses[0]} />);
-  expect(wrapper).toMatchSnapshot();
-});
+let onSubmitFormSpy;
+let onClickSpy;
+let historySpy;
+let wrapper;
 
-test("should onClick be called correctly", () => {
-  const onClickSpy = jest.fn();
-  const historySpy = { push: jest.fn() };
-  const wrapper = shallow(
+beforeEach(() => {
+  onSubmitFormSpy = jest.fn();
+  onClickSpy = jest.fn();
+  historySpy = { push: jest.fn() };
+  wrapper = shallow(
     <EditExpensePage
       onClick={onClickSpy}
       expense={expenses[0]}
       history={historySpy}
+      onSubmitForm={onSubmitFormSpy}
     />
   );
+}, 500);
+test("should render edit expense page correctly", () => {
+  expect(wrapper).toMatchSnapshot();
+});
+
+test("should onClick be called correctly", () => {
   wrapper.find("button").simulate("click");
   expect(onClickSpy).toHaveBeenLastCalledWith(expenses[0].id);
+  expect(historySpy.push).toHaveBeenLastCalledWith("/");
+});
+
+test("should onFormSubmit be called properly", () => {
+  wrapper.find(ExpenseForm).prop("onSubmitForm")(expenses[0]);
+  expect(onSubmitFormSpy).toHaveBeenLastCalledWith(expenses[0].id, expenses[0]);
   expect(historySpy.push).toHaveBeenLastCalledWith("/");
 });
