df9bf0faddbeafc6db1c1cede73914209d413a5e
Akmal <akmalanvaroff@gmail.com>
2020-04-17 19:30:43 +0500

    deleted unused var

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import PropTypes, { oneOfType } from "prop-types";
+import PropTypes from "prop-types";
 import "react-dates/initialize";
 import { DateRangePicker } from "react-dates";
 import {
