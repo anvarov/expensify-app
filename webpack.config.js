626905286168ae7d192377b5baa5cbdd0b904fa1
Akmal <akmalanvaroff@gmail.com>
2020-04-18 15:29:23 +0500

    added config for production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,69 @@
-const path = require('path');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const webpack = require("webpack");
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      }, {
-        test: /\.s?css$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }
-    ]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
+module.exports = (env) => {
+  const isProduction = env === "production";
+  return {
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public", "dist"),
+      filename: "bundle.js",
+    },
+    optimization: {
+      minimize: isProduction,
+      minimizer: [
+        isProduction
+          ? (new OptimizeCssAssetsPlugin(), new TerserPlugin())
+          : new TerserPlugin(),
+      ],
 
+      splitChunks: {
+        cacheGroups: {
+          styles: {
+            name: "main",
+            test: /\.s?css$/,
+            chunks: "all",
+            enforce: true,
+          },
+        },
+      },
+    },
+    plugins: [
+      // new webpack.optimize.OccurenceOrderPlugin(),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+      }),
+      new webpack.DefinePlugin({
+        "process.env": {
+          NODE_ENV: JSON.stringify("production"),
+        },
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/i,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+      ],
+    },
+    devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true,
+      publicPath: "/dist/",
+    },
+  };
 };
-
